76987a4b9de58d537dc4a33167ca503fa0471891
Varun Mangla <manglav@gmail.com>
2015-07-23 13:44:09 -0400

    remove custom param logic. Added sinatra-param gem

--- first.rb
+++ first.rb
@@ -1,34 +1,32 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/param'
 require 'sequel'
 require 'pry-byebug'
-require 'multi_json'
+require 'json'
 
 db_path = 'postgres://gf8gtyxqredf9q6:zd6vgwc3i2zkpgs@hackday.czas5cnicq66.us-east-1.rds.amazonaws.com:5432/farmlogs_rtma_data'
 DB = Sequel.connect(db_path)
-hourly_data_tables = DB.tables.select{|table| table.to_s.start_with?("rtma_data")}
-
 
 class HourlyDatum < Sequel::Model
+  hourly_data_tables = DB.tables.select{|table| table.to_s.start_with?("rtma_data")}
   set_dataset hourly_data_tables.first
 end
 
-get '/data' do
-  clean_params
-  MultiJson.dump(params)
+class App < Sinatra::Base
+  helpers Sinatra::Param
+
+  before do
+    content_type :json
+  end
+
+  get '/data' do
+    param :grid_id, Integer, required: true
+    param :'start-date', Date, required: true
+    param :'end-date', Date, default: lambda { Date.today }
+    params.to_json
+  end
+
+  run! if app_file == $0
 end
 
-def clean_params
-  whitelist_params
-  cast_params
-  validate_params
-end
-
-def whitelist_params
-end
-
-def cast_params
-end
-
-def validate_params
-end
 
