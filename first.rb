7d365ac57f9e5eee70bfe231246e92c9e93db882
Varun Mangla <manglav@gmail.com>
2015-07-23 11:59:01 -0400

    added pry debugger and changed api url

--- first.rb
+++ first.rb
@@ -1,10 +1,12 @@
 require 'sinatra'
 require 'sequel'
+require 'pry-byebug'
 
 db_path = 'postgres://gf8gtyxqredf9q6:zd6vgwc3i2zkpgs@hackday.czas5cnicq66.us-east-1.rds.amazonaws.com:5432/farmlogs_rtma_data'
 DB = Sequel.connect(db_path)
 DB[DB.tables.first]
 
-get '/' do
+get '/data' do
+  binding.pry
   'Hello wofrlsd'
 end
