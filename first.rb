2e1a6f338940a2100a7c6e0c292f7c01c06f68ff
Varun Mangla <manglav@gmail.com>
2015-07-23 12:17:47 -0400

    added model

--- first.rb
+++ first.rb
@@ -1,12 +1,34 @@
 require 'sinatra'
 require 'sequel'
 require 'pry-byebug'
+require 'multi_json'
 
 db_path = 'postgres://gf8gtyxqredf9q6:zd6vgwc3i2zkpgs@hackday.czas5cnicq66.us-east-1.rds.amazonaws.com:5432/farmlogs_rtma_data'
 DB = Sequel.connect(db_path)
-DB[DB.tables.first]
+hourly_data_tables = DB.tables.select{|table| table.to_s.start_with?("rtma_data")}
+hourly_data = DB[hourly_data_tables.first]
+
+class HourlyDatum < Sequel::Model
+
+end
 
 get '/data' do
-  binding.pry
-  'Hello wofrlsd'
+  clean_params
+  MultiJson.dump(params)
 end
+
+def clean_params
+  whitelist_params
+  cast_params
+  validate_params
+end
+
+def whitelist_params
+end
+
+def cast_params
+end
+
+def validate_params
+end
+
